270fe2d0b309322c87c7115b83ad7172138b4880
mohamedlandolsi <mohamedlandolsi300@gmail.com>
2024-10-13 15:22:20 +0100

    Simple auth and user crud, follow and unfollow APIs

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+// Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment
+const mongoose = require("mongoose");
+
+// Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
+const dotenv = require("dotenv");
+
+// Helmet helps you secure your Express apps by setting various HTTP headers
+const helmet = require("helmet");
+
+// Morgan is a middleware that logs HTTP requests
+const morgan = require("morgan");
+
+// Importing routes
+const userRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to MongoDB");
+});
+
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
+app.listen(3000, () => {
+  console.log("Backend server is running on port 3000");
+});
---
+++ routes/users.js
@@ -0,0 +1,104 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// Update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    // If the user wants to update the password
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+
+    // Update the user
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only update your account!");
+  }
+});
+
+// Delete User
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    // Delete the user
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Account has been deleted");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can only update your account!");
+  }
+});
+
+// Get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Follow a user
+router.put("/:id/follow", async (req, res) => {
+  // If the user is not the same as the one who is following
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      // If the user is not already followed
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json(`User ${user.username} has been followed`);
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can't follow yourself");
+  }
+});
+
+// Unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  // If the user is not the same as the one who is unfollowing
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      // If the user is already followed
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json(`User ${user.username} has been unfollowed`);
+      } else {
+        res.status(403).json("You don't follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can't unfollow yourself");
+  }
+});
+
+module.exports = router;
